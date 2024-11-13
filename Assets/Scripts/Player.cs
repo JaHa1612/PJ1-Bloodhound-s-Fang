f7b6038ee8f7a16ddeba0d4f2d2599aeadcf3f7e
JaHa1612 <luonggiahao04@gmail.com>
2024-11-13 17:58:04 +0700

    setupGit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,157 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Dash info")]
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private float dashDuration;
+     private float dashTime;
+    [SerializeField] private float dashCoolDown;
+     private float dashCoolDownTimer;
+    [SerializeField] private float dashtime;
+
+    private float xInput;
+    private bool isMoving;
+
+    private int facingDir = 1;
+    private bool facingRight = true;
+
+    private bool isGrounded;
+
+    [Header("Collision info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        AnimatorController();
+        CollisionCheck();
+
+        dashTime -= Time.deltaTime;
+        dashCoolDownTimer -= Time.deltaTime;
+
+
+
+        CheckInput();
+        FlipController();
+
+    }
+
+    private void CollisionCheck()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void CheckInput()
+    {
+        xInput = UnityEngine.Input.GetAxisRaw("Horizontal");
+
+        if (UnityEngine.Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            DashAbility();
+        }
+    }
+
+    private void DashAbility()
+    {
+        if (dashCoolDownTimer < 0)
+        {
+            dashCoolDownTimer = dashCoolDown;
+            dashTime = dashDuration;
+        }
+    }
+
+    private void Movement()
+    {
+        if (dashTime > 0)
+        {
+            rb.velocity = new Vector2(xInput * dashSpeed, 0);
+
+        }
+        else
+        {
+            rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+        }
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AnimatorController()
+    {
+
+        isMoving = rb.velocity.x != 0;
+
+        anim.SetFloat("Jump/Fall", rb.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isMoving", isMoving);
+        anim.SetBool("isDashing", dashTime > 0);
+    }
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+    private void FlipController()
+    {
+        if (rb.velocity.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (rb.velocity.x < 0 && facingRight)
+        {
+            Flip();
+        }
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("takePower"))
+        {
+            StartCoroutine(TemporaryDashSpeedBoost());
+        }
+    }
+
+    private IEnumerator TemporaryDashSpeedBoost()
+    {
+        float originalDashSpeed = dashSpeed; // Store the original speed
+        dashSpeed = dashSpeed * 10; // Increase the speed
+
+        yield return new WaitForSeconds(dashtime); // Wait for 1 second
+
+        dashSpeed = originalDashSpeed; // Revert back to the original speed
+    }
+
+
+}
